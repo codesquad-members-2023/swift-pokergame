684659de55f331de2257f3392c80ee365936a0f0
wnsqhs <wnsqhs7804@gmail.com>
2023-03-13 16:58:36 +0900

    프로젝트 생성

---
+++ PokerGameApp/PokerGameApp/SceneDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  SceneDelegate.swift
+//  PokerGameApp
+//
+//  Created by PJB on 2023/03/13.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    var window: UIWindow?
+}
+
