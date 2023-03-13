9b901c049a31c58cf4ad6544073dd267523258ea
Haena <ghkwlsspdl@naver.com>
2023-03-13 21:55:26 +0900

    코드로 7개 UIImageView를 생성 후 카드뒷면 사진 첨부. 비율조정

--- PokerGame/PokerGame/ViewController.swift
+++ PokerGame/PokerGame/ViewController.swift
@@ -8,13 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let backgroundImage = UIImage(named: "bg_pattern")
-        self.view.backgroundColor = UIColor(patternImage: backgroundImage!)
+        guard let backgroundImage = UIImage(named: "bg_pattern") else {
+            print("배경이미지 오류.")
+            return}
+        self.view.backgroundColor = UIColor(patternImage: backgroundImage)
+        
+        let cardBackImage = UIImage(named: "card-back")
+        var cardLocateX: CGFloat = 2.5
+        var cardLocateY: CGFloat = 60
+        let cardWidth = (self.view.frame.width / 7)-5
+        let cardHeight = cardWidth * 1.27
+        for _ in 1...7 {
+            let cardImageView = UIImageView(image: cardBackImage)
+            cardImageView.frame = CGRect(x: cardLocateX, y: cardLocateY, width: cardWidth, height: cardHeight)
+            self.view.addSubview(cardImageView)
+            cardLocateX += (cardWidth+5)
+        }
     }
-    
 }
+
+
 
--- README.md
+++ README.md
@@ -5,10 +5,9 @@
 학습파일 바로가기 >>>> https://github.com/boriiiborii/baekjun/files/10954769/STUDY.md
 - - -
 # ❗️체크리스트❗️
-1-1
+2-1
 - [x] ViewController 클래스에서 self.view 배경을 다음 이미지 패턴으로 지정. 이미지 파일은 Assets에 추가
 <img src="https://user-images.githubusercontent.com/97685264/224651784-b7b55fce-70bb-4f31-b9f1-eba36af76641.png">
-- [ ] 다음 카드 뒷면 이미지를 다운로드해서 프로젝트 Assets.xcassets에 추가
-- [ ] ViewController 클래스에서 코드로 7개 UIImageView를 생성하고, 카드뒷면 이미지 연결
-- [ ] 카드 가로와 세로 비율은 1:1.27로 지정
+- [x] ViewController 클래스에서 코드로 7개 UIImageView를 생성하고, 카드뒷면 이미지 연결
+- [x] 카드 가로와 세로 비율은 1:1.27로 지정
 - [ ] 앱 기본 설정(Info.plist)을 변경하는 방식에 대해 학습하고 앱 표시 이름을 변경
