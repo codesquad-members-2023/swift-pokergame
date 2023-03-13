eed7f53679fb36dbbdc253bb80ff4cb6649b898e
Hyejeong Park <effie.thedev@gmail.com>
2023-03-13 10:43:29 +0900

    [step 1] Create and Initialize project
    -  프로젝트 생성
    - 불필요한 코드 제거

---
+++ PokerGameApp/PokerGameApp/SceneDelegate.swift
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.swift
+//  PokerGameApp
+//
+//  Created by Effie on 2023/03/13.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+  var window: UIWindow?
+  
+}
+
---
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  PokerGameApp
+//
+//  Created by Effie on 2023/03/13.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+
+}
+
