ba5d393d4f3f78cb3442e61ae0b3c21ea85fdca4
SONG <geun2121@gmail.com>
2023-03-13 17:39:15 +0900

    Set Cards
    - func setCardBack () 추가

--- PokerGameApp/PokerGameApp/GameBoardViewController.swift
+++ PokerGameApp/PokerGameApp/GameBoardViewController.swift
@@ -12,6 +12,7 @@ class GameBoardViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setBackground()
+        setCardBack()
     }
     
     func setBackground () {
@@ -23,6 +24,23 @@ class GameBoardViewController: UIViewController {
         self.view.backgroundColor = UIColor(patternImage: pattern)
     }
     
-
+    func setCardBack () {
+        var cardBack : UIImage? = UIImage(named: "card-back")
+        
+        guard let img = cardBack else {
+            return
+        }
+        let boundaryWidth = self.view.bounds.width
+        let cardWidth = boundaryWidth/7
+        var posX = 0.0
+        
+        for _ in 0...6 {
+            var cardBackView = UIImageView(image: img)
+            cardBackView.frame = CGRect(x: posX+2.0 , y: 50, width: cardWidth-4.0, height: cardWidth * 1.25 )
+            self.view.addSubview(cardBackView)
+            posX += cardWidth
+        }
+        
+    }
 }
 
