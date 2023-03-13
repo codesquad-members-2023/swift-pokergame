eda2df358cdfd6f00e05c14e23d6033f15c5fd4c
Hyejeong Park <effie.thedev@gmail.com>
2023-03-13 15:29:49 +0900

    [step 1] Set ViewController background color with a asset image
    - 패턴 asset 추가
    - vc 배경 컬러를 init(patterImage:)를 설정하는 configure 메소드 구현

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -11,7 +11,12 @@ class ViewController: UIViewController {
 
   override func viewDidLoad() {
     super.viewDidLoad()
+    configure()
+  }
+  
+  private func configure() {
+    guard let pattermImage = UIImage(named: "bg_pattern") else { return }
+    view.backgroundColor = .init(patternImage: pattermImage)
   }
 
 }
-
