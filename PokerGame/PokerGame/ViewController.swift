507840b60e8d592b7507198f03b0726f1cbccfdc
Haena <ghkwlsspdl@naver.com>
2023-03-14 11:03:40 +0900

    step2-1 코드리뷰 반영
    프로젝트 생성 이후에 만들어지는 코드 중 필요없는 코드 삭제
    배경이미지가 없더라도 앱이 꺼지지 않고 작업을 할 수 있도록 개선
    의미가 있는 숫자들 변수처리

--- PokerGame/PokerGame/ViewController.swift
+++ PokerGame/PokerGame/ViewController.swift
@@ -11,22 +11,28 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        guard let backgroundImage = UIImage(named: "bg_pattern") else {
+        printCardImage()
+    }
+    
+    func printCardImage() {
+        guard let backgroundImage = UIImage(named: "bg_pattern")else {
             print("배경이미지 오류.")
-            return}
+            return
+        }
         self.view.backgroundColor = UIColor(patternImage: backgroundImage)
-        
         let cardBackImage = UIImage(named: "card-back")
         var cardLocateX: CGFloat = 2.5
-        var cardLocateY: CGFloat = 60
-        let cardWidth = (self.view.frame.width / 7)-5
-        let cardHeight = cardWidth * 1.27
+        let cardLocateY: CGFloat = 60
+        let ratio = 1.27
+        let numberCards: CGFloat = 7
+        let spacingCard: CGFloat = 5
+        let cardWidth = (self.view.frame.width / numberCards)-spacingCard
+        let cardHeight = cardWidth * ratio
         for _ in 1...7 {
             let cardImageView = UIImageView(image: cardBackImage)
             cardImageView.frame = CGRect(x: cardLocateX, y: cardLocateY, width: cardWidth, height: cardHeight)
             self.view.addSubview(cardImageView)
-            cardLocateX += (cardWidth+5)
+            cardLocateX += (cardWidth+spacingCard)
         }
     }
 }
