bfd16adc14f8fb76d8323e936312fdd5fc1fc096
dpfdlalfm <84387335+dpfdlalfm@users.noreply.github.com>
2023-03-14 10:54:16 +0900

    불필요한 생성 줄이기 위해 UIImage 변수 설정[2-1 피드백]

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -8,15 +8,17 @@ class GameViewController: UIViewController {
         }
         
         let cardWidth = UIScreen.main.bounds.size.width / 7 - 3
+        let cardImage = UIImage(named: "card-back")
         for i in 0..<7 {
-            let card = UIImageView(image: UIImage(named: "card-back"))
+            let card = UIImageView(image: cardImage)
             card.frame = CGRect(x: 2.6 + CGFloat(i)*(cardWidth + 2.6), y: 54,
                                 width: cardWidth, height: cardWidth * 1.27)
             // 카드간 간격인 2.6은 임의로 설정한 값입니다.
             // y값은 아래 문서에서 발췌한 IPhone 14의 Safe Area 높이입니다.
             // https://useyourloaf.com/blog/iphone-14-screen-sizes/
-            
             self.view.addSubview(card)
         }
     }
 }
+
+
