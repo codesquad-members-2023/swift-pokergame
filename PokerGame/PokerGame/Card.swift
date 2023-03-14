bbf2895a670cc0a4e776e4c00c85005135c6f4f6
Haena <ghkwlsspdl@naver.com>
2023-03-15 00:41:18 +0900

    step2-2 코드리뷰 반영
    makeCard 클래스 >> Card 클래스로 수정
    card클래스의 number변수 enum타입으로 지정
    card클래스의 이미지 변수 삭제
    card클래스의 이모티콘은 shap변수의 이넘타입(로우밸류)를 통하여 선언
    card메서드 description 구현. >> print형식이 아닌 return형식으로 빼주었으며 print는 viewDidLoad에서 실행되도록 작성
    이미지는 추후 미션에 있으므로 다시 카드 뒷면으로 돌려놓음

---
+++ PokerGame/PokerGame/Card.swift
@@ -0,0 +1,53 @@
+//
+//  MakeCard.swift
+//  PokerGame
+//
+//  Created by leehwajin on 2023/03/14.
+//
+
+import Foundation
+
+class Card{
+    //enum타입 한 이유 : 어짜피 모양은 4개중 하나, 넘버는 1~13중 하나라서
+    var shape : Shape
+    var number : Number
+    lazy var imoticon: String = self.shape.rawValue
+
+    init(shape: Shape, number: Number) {
+        self.shape = shape
+        self.number = number
+    }
+    
+    enum Shape: String{
+        case heart = "❤️"
+        case spade = "♠️"
+        case clover = "♣️"
+        case diamond = "◆"
+    }
+    
+    enum Number : String{
+        case one = "A"
+        case two = "2"
+        case three = "3"
+        case four = "4"
+        case five = "5"
+        case six = "6"
+        case seven = "7"
+        case eight = "8"
+        case nine = "9"
+        case ten = "10"
+        case eleven = "J"
+        case twelve = "Q"
+        case thirteen = "K"
+    }
+    
+    func description() -> String{
+        return self.imoticon + self.number.rawValue
+    }
+}
+
+
+
+
+
+
