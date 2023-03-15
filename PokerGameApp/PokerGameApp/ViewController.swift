77cd21dc5bf361a969a35458801080ce9cd09ebf
demiiann <tkim0156@gmail.com>
2023-03-16 03:28:50 +0900

    feat : 카드 생성 코드 추가
    - 반복문으로 7장의 카드를 생성
    - 기존 배경화면 생성 코드 메서드화

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -11,8 +11,29 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        makeBackgroundView()
+        let interval = 4
+        let cardWidth = self.view.bounds.width/7 - 3.4
+        for i in 0...6 {
+            let cardImage = makeCardView(posX: Int(interval) + (Int(interval) + Int(cardWidth)) * i)
+            self.view.addSubview(cardImage)
+        }
+    }
+    
+    func makeBackgroundView() {
         guard let backgroundImage = UIImage(named: "background") else { return }
         self.view.backgroundColor = UIColor(patternImage: backgroundImage)
+    }
+    
+    func makeCardView(posX: Int) -> UIImageView {
+        let width: CGFloat = self.view.bounds.width/7 - 3.4
+        let height: CGFloat = width * 1.27
+        let posY: CGFloat = 55
+        
+        let imageView = UIImageView(frame: CGRect(x: CGFloat(posX), y: posY, width: width, height: height))
+        let myImage: UIImage = UIImage(named: "card_back")!
+        imageView.image = myImage
+        return imageView
     }
 }
 
