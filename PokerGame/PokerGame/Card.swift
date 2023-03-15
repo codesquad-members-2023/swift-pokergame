cd31937217fe09bd4c2f4c4c1324e774a53899db
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-15 20:59:54 +0900

    Deck에 메소드 추가
    count
    shuffle
    removeOne
    reset

--- PokerGame/PokerGame/Card.swift
+++ PokerGame/PokerGame/Card.swift
@@ -22,14 +22,14 @@ class Card {
 
 // Shape, Rank에는 값이 한정되어 있으므로 enum이 적절하다고 판단했습니다. 또 Rank의 경우에서 one, eleven, tweleve, thirteen에서 숫자가 아닌 문자로 표현하는 데에 있어 enum의 rawValue를 활용하는 것이 적절하다고 생각했습니다.
 
-enum Shape: String {
+enum Shape: String, CaseIterable {
     case Space = "♠️"
     case Clover = "♣️"
     case Diamond = "♦️"
     case Heart = "♥️"
 }
 
-enum Rank: String {
+enum Rank: String, CaseIterable {
     case one = "A"
     case two = "2"
     case three = "3"
--- PokerGame/PokerGame/Deck.swift
+++ PokerGame/PokerGame/Deck.swift
@@ -8,5 +8,41 @@
 import Foundation
 
 struct Deck {
-    var cards: [Card]
+    private var cards: [Card]
+    
+    func count() -> Int {
+        cards.count
+    }
+    
+    mutating func shuffle() {
+        // 무작위 인덱스 생성 : 0...cards개수-1을 담은 배열, randomElement로 하나 빼고, remove를 통해 뺀 것 제거..
+        // 제거한 인덱스로 cards에 접근, result: [Cards]에 추가
+        // 인덱스를 추출하는 배열이 빌 때까지 이 작업을 반복한다
+        // cards에 result를 할당한다
+        // 끝
+        // O(n)
+        var arr = Array(0..<cards.count)
+        var result: [Card] = .init()
+        
+        while arr.count != 0 {
+            let randomIndex = arr.randomElement()!
+            let removeIdx = arr.firstIndex(of: randomIndex)!
+            arr.remove(at: removeIdx)
+            result.append(cards[randomIndex])
+        }
+        cards = result
+    }
+    
+    mutating func removeOne() -> Card? {
+        cards.popLast()
+    }
+    
+    mutating func reset() {
+        for shape in Shape.allCases {
+            for rank in Rank.allCases {
+                let card = Card(shape: shape, rank: rank)
+                cards.append(card)
+            }
+        }
+    }
 }
