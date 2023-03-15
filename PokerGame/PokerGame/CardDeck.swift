7df0651c71de1a8a51e96c793d513b6f20ad5684
Haena <ghkwlsspdl@naver.com>
2023-03-15 16:00:11 +0900

    다음 인터페이스만 보이도록 구현. count shuffle removeOne reset
    shuffle은 스위프트에서 제공하는 shuffle함수가 아닌 Fisher-Yates을 참조하여 구현하였음

--- PokerGame/PokerGame/CardDeck.swift
+++ PokerGame/PokerGame/CardDeck.swift
@@ -4,12 +4,11 @@
 //
 //  Created by leehwajin on 2023/03/15.
 //
-
 import Foundation
 
-struct cardDeck{
+struct CardDeck{
     
-    private var cardArray : [Card]
+    var cardArray : [Card]
     
     init() {
         cardArray = []
@@ -20,4 +19,40 @@ struct cardDeck{
             }
         }
     }
+    
+    mutating func shuffle() {
+        // 전체 카드를 랜덤하게 섞는다.
+        // Fisher-Yates 사용
+        for i in 0..<cardArray.count {
+            if let randomIndex = (i+1..<cardArray.count).randomElement() {
+                let changeCard = cardArray[randomIndex]
+                cardArray[randomIndex] = cardArray[i]
+                cardArray[i] = changeCard
+            }
+        }
+    }
+    
+    mutating func removeOne() -> Card{
+        //카드 인스턴스 중에 하나를 반환하고 목록에서 삭제
+        let cardBeDeleted = cardArray.remove(at: Int((0..<cardArray.count).randomElement()!))
+        return cardBeDeleted
+    }
+    
+    mutating func reset(){
+        //처음처럼 모든 카드를 다시 채워넣는다.
+        cardArray.removeAll()
+        for shape in Card.Shape.allCases {
+            for number in Card.Number.allCases {
+                let card = Card(shape: shape, number: number)
+                cardArray.append(card)
+            }
+        }
+    }
+    
+    mutating func count() -> Int{
+        return cardArray.count
+    }
 }
+
+var cardDeck = CardDeck()
+
--- PokerGame/PokerGame/ViewController.swift
+++ PokerGame/PokerGame/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
         printCardImage()
         
         let card = Card(shape: .heart, number: .two)
-        print(card)
+        print(card.description())
     }
     
     func printCardImage() {
