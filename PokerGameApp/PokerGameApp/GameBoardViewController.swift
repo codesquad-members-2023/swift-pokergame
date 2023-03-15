a6ca300d7f8cc137e7f1531a1248d46513b60be2
SONG <geun2121@gmail.com>
2023-03-15 11:34:03 +0900

    Reflect feedback
    - 변수 선언과 옵셔널 처리를 동시에 작성 
    - pos -> position 으로 변수명 변경

--- PokerGameApp/PokerGameApp/GameBoardViewController.swift
+++ PokerGameApp/PokerGameApp/GameBoardViewController.swift
@@ -20,32 +20,29 @@ class GameBoardViewController: UIViewController {
     }
     
     func setBackground () {
-        let backgroundPattern : UIImage? = UIImage(named: "bg_pattern")
-        
-        guard let pattern = backgroundPattern else {
+        guard let backgroundPattern = UIImage(named: "bg_pattern") else {
             return
         }
-        self.view.backgroundColor = UIColor(patternImage: pattern)
+        self.view.backgroundColor = UIColor(patternImage: backgroundPattern)
     }
     
     func setCardBack () {
-        let cardBack : UIImage? = UIImage(named: "card-back")
         let boundaryWidth = self.view.bounds.width
-        var posX = 2.0
-        let posY = 50.0
+        var positionX = 2.0
+        let positionY = 50.0
         let padding = 2.0
         let cardWidth = boundaryWidth/7 - (2*padding)
         let cardHeight = cardWidth * 1.27
         
-        guard let img = cardBack else {
+        guard let cardBack = UIImage(named: "card-back") else {
             return
         }
         
         for _ in 0...6 {
-            let cardBackView = UIImageView(image: img)
-            cardBackView.frame = CGRect(x: posX , y: posY, width: cardWidth, height: cardHeight)
+            let cardBackView = UIImageView(image: cardBack)
+            cardBackView.frame = CGRect(x: positionX , y: positionY, width: cardWidth, height: cardHeight)
             self.view.addSubview(cardBackView)
-            posX += cardWidth + (2*padding)
+            positionX += cardWidth + (2*padding)
         }
     }
 }
