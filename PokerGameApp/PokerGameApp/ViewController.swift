3436f11e7df4f21c12d58a7e64e59351087990ff
HansolWorld <hs9269@gmail.com>
2023-03-16 15:36:20 +0900

    add background image

--- PokerGameApp/PokerGameApp/ViewController.swift
+++ PokerGameApp/PokerGameApp/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let backgroundImage = UIImage(named: "backGround")
+        self.view.backgroundColor = UIColor(patternImage: backgroundImage!)
     }
-
-
 }
 
