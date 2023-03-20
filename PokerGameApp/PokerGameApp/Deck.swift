168c64b1e29a44f20fae04595e2f3ffb85921395
SONG <geun2121@gmail.com>
2023-03-20 12:24:57 +0900

    Update func removeOne() , shuffle()
    - 불필요한 return 문 제거 
    - guard else 문으로 코드의 뎁스 조절

--- PokerGameApp/PokerGameApp/Deck.swift
+++ PokerGameApp/PokerGameApp/Deck.swift
@@ -4,7 +4,7 @@ import Foundation
 
 struct Deck {
    var deck : [Card] = []
-
+    
     func count() -> Int {
         return self.deck.count
     }
@@ -17,18 +17,20 @@ struct Deck {
         }
     }
     mutating func removeOne() -> Card? {
-        if let drawedCard = self.deck.popLast() {
-            return drawedCard
+        guard self.deck.isEmpty == false else {
+            return nil
         }
-        return nil
+        return self.deck.popLast()
     }
     
     mutating func shuffle() {
-        if self.deck.isEmpty == false {
-            for i in stride(from: self.deck.count - 1, to: 0, by: -1) {
-                let j = Int.random(in: 0...i)
-                deck.swapAt(i, j)
-            }
+        guard self.deck.isEmpty == false else {
+            return
+        }
+        
+        for i in stride(from: self.deck.count - 1, to: 0, by: -1) {
+            let j = Int.random(in: 0...i)
+            deck.swapAt(i, j)
         }
     }
 }
