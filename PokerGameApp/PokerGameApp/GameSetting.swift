e96353c3084c0f11eb558771523c13c5d8c19e0f
SONG <geun2121@gmail.com>
2023-03-20 16:53:58 +0900

    Update class GameSetting
    - enum Mode 내부에 5스터드 , 7스터드 게임모드 분기

---
+++ PokerGameApp/PokerGameApp/GameSetting.swift
@@ -0,0 +1,16 @@
+//
+//  GameSetting.swift
+//  PokerGameApp
+//
+//  Created by SONG on 2023/03/20.
+//
+
+import Foundation
+
+class GameSetting {
+    enum Mode {
+        case fiveStud
+        case sevenStud
+        
+    }
+}
