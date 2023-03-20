0bb35f81df5a203d5fdb3f5f540acd2eac23107a
Hyejeong Park <effie.thedev@gmail.com>
2023-03-20 12:25:02 +0900

    [add] Add CardDeck
    - CardDeck 구조체 구현
    - deck 생성을 위해 Card.Number, Card.Shape에 CaseIterable 프로토콜 채용
    - deck 구현을 위한 Array 확장

---
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -0,0 +1,44 @@
+//
+//  CardDeck.swift
+//  PokerGameApp
+//
+//  Created by Effie on 2023/03/20.
+//
+
+import Foundation
+
+struct CardDeck {
+  private var cards: [Card]
+  
+  var count: Int {
+    cards.count
+  }
+  
+  init() {
+    self.cards = Self.createNewDeck()
+  }
+  
+  private static func createNewDeck() -> [Card] {
+    var newCards = [Card]()
+    for number in Card.Number.allCases {
+      for shape in Card.Shape.allCases {
+        let newCard = Card(shape: shape, number: number)
+        newCards.append(newCard)
+      }
+    }
+    return newCards
+  }
+  
+  mutating func shuffle() {
+    cards.myShuffle()
+  }
+  
+  mutating func removeOne() -> Card? {
+    cards.removeRandom()
+  }
+  
+  mutating func reset() {
+    cards = Self.createNewDeck()
+  }
+}
+
