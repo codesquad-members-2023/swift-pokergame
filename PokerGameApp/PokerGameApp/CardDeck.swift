563dd607bd7c94f39a4e6ae2a46e07d1815b31b0
demiiann <tkim0156@gmail.com>
2023-03-20 14:19:26 +0900

    feat : 카드 섞어주는 shuffle 함수 구현
    - knuth shuffle 구현

--- PokerGameApp/PokerGameApp/CardDeck.swift
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -14,5 +14,12 @@ struct CardDeck {
         print("\n=> 총 \(cardList.count)장의 카드가 남아 있습니다.\n")
     }
     
-    
+    mutating func shuffle() { // Knuth Shuffle 구현
+        print("> 카드 섞기 \n\(cardList.count)장의 카드를 섞었습니다.\n")
+        for i in 0 ..< (cardList.count - 1) {
+            let randomIndex = Int.random(in: i..<cardList.count)
+            cardList.swapAt(i, randomIndex)
+        }
+        printCardList(cardList: cardList)
+    }
 }
