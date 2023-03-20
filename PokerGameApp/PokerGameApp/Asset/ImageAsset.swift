538990707117eac8ebadc78b893f9c964427c5d9
noah0316 <dev.noah0316@gmail.com>
2023-03-20 11:33:51 +0900

    ImageAsset 관리를 위한 열거형 추가

---
+++ PokerGameApp/PokerGameApp/Asset/ImageAsset.swift
@@ -0,0 +1,18 @@
+//
+//  ImageAsset.swift
+//  PokerGameApp
+//
+//  Created by Noah on 2023/03/16.
+//
+
+import UIKit.UIImage
+
+enum ImageAsset: String {
+    case pokerBoard = "PokerBoard"
+}
+
+extension ImageAsset {
+    static func load(from imageAsset: ImageAsset) -> UIImage? {
+        return UIImage(named: imageAsset.rawValue)
+    }
+}
