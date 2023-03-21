26f12a416c9df8e554fd7428cc8db8b65be7cdaf
wnsqhs <wnsqhs7804@gmail.com>
2023-03-21 15:43:02 +0900

    데이터 구조를 선택한 이유 주석 작성

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -8,7 +8,10 @@
 import Foundation
 
 struct Card {
+    // struct로 구현한 이유는 포커를 구현하는데, Card는 상속이 필요 없다고 생각했습니다
     
+    // Enum을 사용한 이유는 서로 연관된 데이터라고 생각했습니다
+    // 그리고 카드의 경우 문양과 숫자의 갯수가 정해져있고, 이미 정해놓은 입력값만 받고 싶기 때문에 Enum을 사용하였습니다
     enum Suit: Character, CustomStringConvertible {
         case spades = "♠"
         case hearts = "♥"
