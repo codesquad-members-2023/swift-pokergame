7e7fcd6b5759b643d53d643c89f5ecb124821bf7
wnsqhs <wnsqhs7804@gmail.com>
2023-03-21 15:00:00 +0900

    Rank enum 생성 및 원시값 비교 후 변환
    카드의 숫자를 나타내는 Rank 열거형 생성
    요구사항에 알맞게 switch 문을 통해서 값 변환

--- PokerGameApp/PokerGameApp/Card.swift
+++ PokerGameApp/PokerGameApp/Card.swift
@@ -19,4 +19,34 @@ struct Card {
             return "\(self.rawValue)"
         }
     }
+    enum Rank: Int, CustomStringConvertible {
+        case ace = 1
+        case two = 2
+        case three = 3
+        case four = 4
+        case five = 5
+        case six = 6
+        case seven = 7
+        case eight = 8
+        case nine = 9
+        case ten = 10
+        case jack = 11
+        case queen = 12
+        case king = 13
+        
+        var description: String {
+            switch self {
+            case .ace:
+                return "A"
+            case .jack:
+                return "J"
+            case .queen:
+                return "Q"
+            case .king:
+                return "K"
+            default:
+                return "\(self.rawValue)"
+            }
+        }
+    }
 }
