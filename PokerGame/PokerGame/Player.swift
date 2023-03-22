d71554da6cb18e87e2fe69f8077314068eb9859b
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-22 15:48:24 +0900

    Player에 name, cards 프라퍼티 추가
    availableNames, generateRandomName을 통해 초기화시 랜덤으로 이름 할당

--- PokerGame/PokerGame/Player.swift
+++ PokerGame/PokerGame/Player.swift
@@ -8,5 +8,19 @@
 import Foundation
 
 class Player {
+    var name: String
+    var cards: [Card]
     
+    init() {
+        name = Player.generateRandomName()
+        cards = []
+    }
+}
+
+extension Player {
+    static var availableNames: [String] = ["crong", "honux", "jk", "하림", "haena", "effie", "wood", "Aiden", "Noah", "울버린", "에디"].shuffled()
+    
+    private static func generateRandomName() -> String {
+        Player.availableNames.popLast() ?? "Unnamed"
+    }
 }
