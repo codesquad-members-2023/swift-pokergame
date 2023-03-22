aade1738e0e1cefb1233e57dcfd7a54d205ba142
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-22 23:11:04 +0900

    Deck의 타입을 Class로 변경

--- PokerGame/PokerGame/Deck.swift
+++ PokerGame/PokerGame/Deck.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Deck: CardDeck {
+class Deck: CardDeck {
     var cards: [Card]
     
     init() {
@@ -19,7 +19,7 @@ struct Deck: CardDeck {
         cards.count
     }
     
-    mutating func shuffle() {
+    func shuffle() {
         // 무작위 인덱스 생성 : 0...cards개수-1을 담은 배열, randomElement로 하나 빼고, remove를 통해 뺀 것 제거..
         // 제거한 인덱스로 cards에 접근, result: [Cards]에 추가
         // 인덱스를 추출하는 배열이 빌 때까지 이 작업을 반복한다
@@ -41,12 +41,12 @@ struct Deck: CardDeck {
         cards = result
     }
     
-    mutating func removeOne() -> Card? {
+    func removeOne() -> Card? {
         cards.popLast()
     }
     
     // 모든 카드를 덱에 담는 함수. 순서가 정해져 있다.
-    mutating func reset() {
+    func reset() {
         for shape in Shape.allCases {
             for rank in Rank.allCases {
                 let card = Card(shape: shape, rank: rank)
