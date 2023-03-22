08d03472e816ae975579d1922b48f5ba5d963393
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-22 14:41:33 +0900

    PokerGame, Dealer 파일 추가

---
+++ PokerGame/PokerGame/Dealer.swift
@@ -0,0 +1,17 @@
+//
+//  Dealer.swift
+//  PokerGame
+//
+//  Created by 에디 on 2023/03/22.
+//
+
+import Foundation
+
+class Dealer {
+    
+    var pokerGameDelegate: PokerGame?
+    
+    func switchGameStyle() {
+        
+    }
+}
---
+++ PokerGame/PokerGame/PokerGame.swift
@@ -0,0 +1,17 @@
+//
+//  PokerGame.swift
+//  PokerGame
+//
+//  Created by 에디 on 2023/03/22.
+//
+
+import Foundation
+
+class PokerGame {
+    
+}
+
+enum GameStyle {
+    case FiveCardStud
+    case SevenCardStud
+}
