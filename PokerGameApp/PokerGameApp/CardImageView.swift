c1434af4d23bc736d3c2ae8b4c12697ce11150b4
noah0316 <dev.noah0316@gmail.com>
2023-03-22 11:54:29 +0900

    CardImageView convenience init 추가
    - image, width, origin을 인자로 받아 CardImageView를 생성하는 convenience init 추가
    - 인자로 받은 width와 상수로 가지고 있는 heightScaleRatio를 이용해 height 계산

--- PokerGameApp/PokerGameApp/CardImageView.swift
+++ PokerGameApp/PokerGameApp/CardImageView.swift
@@ -18,4 +18,14 @@ final class CardImageView: UIImageView {
     required init?(coder: NSCoder) {
         super.init(coder: coder)
     }
+    
+    convenience init(image: UIImage?, width: CGFloat, origin: CGPoint) {
+        self.init(image: image)
+        let size = CGSize(
+            width: width,
+            height: width * self.heightScaleRatio
+        )
+        self.frame = CGRect(origin: origin, size: size)
+        self.contentMode = .scaleAspectFit
+    }
 }
