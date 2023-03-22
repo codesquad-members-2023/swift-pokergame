a2d9e2fd108397e38c764fd89e0e2ec5254cf7d2
SONG <geun2121@gmail.com>
2023-03-22 14:44:30 +0900

    Create PokerGameAppTests

---
+++ PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
@@ -0,0 +1,26 @@
+//
+//  PokerGameAppTests.swift
+//  PokerGameAppTests
+//
+//  Created by SONG on 2023/03/22.
+//
+
+import XCTest
+
+final class PokerGameAppTests: XCTestCase {
+
+    override func setUpWithError() throws {
+    }
+
+    override func tearDownWithError() throws {
+    }
+
+    func testExample() throws {
+    }
+
+    func testPerformanceExample() throws {
+        measure {
+        }
+    }
+
+}
