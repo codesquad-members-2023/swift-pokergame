afa5a4d15cad20a86cd17090976ce5d4d03c1d01
noah0316 <dev.noah0316@gmail.com>
2023-03-22 11:36:24 +0900

    CardImageView 파일 추가
    - Card UI를 표시하기 위한 UIImageView 서브클래스 추가

---
+++ PokerGameApp/PokerGameApp/CardImageView.swift
@@ -0,0 +1,19 @@
+//
+//  CardImageView.swift
+//  PokerGameApp
+//
+//  Created by Noah on 2023/03/20.
+//
+
+import UIKit
+
+final class CardImageView: UIImageView {
+    
+    override init(image: UIImage?) {
+        super.init(image: image)
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+}
