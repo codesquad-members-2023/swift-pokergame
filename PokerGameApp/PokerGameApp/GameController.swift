efbf5036ee4d309d58b55fc4f75d4260775cd278
SONG <geun2121@gmail.com>
2023-03-23 19:06:50 +0900

    Update GameController class
    - 게임세팅 -> 딜러 일해 -> 츨력 역할

--- PokerGameApp/PokerGameApp/GameController.swift
+++ PokerGameApp/PokerGameApp/GameController.swift
@@ -7,5 +7,17 @@
 
 import Foundation
 
-class GameController {
+class GameController  {
+    
+    func run( mode : GameSetting.Mode , numberOfParticipant : Int ) {
+        let gameSetting = GameSetting(mode, numberOfParticipant)
+        gameSetting.setPlayer()
+        
+        let dealer : Dealer = gameSetting.playerList[numberOfParticipant] as! Dealer
+        dealer.work()
+        
+        for player in gameSetting.playerList {
+            print(player)
+        }
+    }
 }
