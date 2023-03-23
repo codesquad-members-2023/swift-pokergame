5f09f8ac576a2cc3e552bc3c88bfc485ec5a2726
raindropiOS <70703326+raindropiOS@users.noreply.github.com>
2023-03-23 17:28:40 +0900

    PokerGame 테스트 코드에 startGame 테스트를 코드 추가
    testPokerGame_WhenUsingStartingGame_ShouldGivePlayers_Adequate_Number_Of_Cards 추가

--- PokerGame/PokerGame_EddieTests/PokerGame_EddieTests.swift
+++ PokerGame/PokerGame_EddieTests/PokerGame_EddieTests.swift
@@ -10,7 +10,8 @@ import XCTest
 final class PokerGame_EddieTests: XCTestCase {
     private var sut: PokerGame = .init()
     
-    override func setUpWithError() throws {}
+    override func setUpWithError() throws { }
+    
     override func tearDownWithError() throws {}
 
     func testExample() throws {
@@ -22,10 +23,26 @@ final class PokerGame_EddieTests: XCTestCase {
         XCTAssert(true)
     }
     
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        measure {
-            // Put the code you want to measure the time of here.
+    func testPokerGame_WhenUsingStartingGameWithFourPlayers_ShouldGivePlayers_Adequate_Number_Of_Cards() throws {
+//        let dealer = Dealer()
+//        dealer.pokerGameDelegate = sut
+//        sut.dealer = dealer
+        sut.startGame(playerCount: 4)
+        
+        if sut.gameStyle == .FiveCardStud {
+            for player in sut.players {
+                let numberOfCards = player.cards.count
+                guard numberOfCards == 5 else { return XCTAssert(false, "Player got \(numberOfCards) cards. It must be 5")}
+            }
         }
+        
+        if sut.gameStyle == .SevenCardStud {
+            for player in sut.players {
+                let numberOfCards = player.cards.count
+                guard numberOfCards == 7 else { return XCTAssert(false, "Player got \(numberOfCards) cards. It must be 7")}
+            }
+        }
+        
+        XCTAssert(true)
     }
 }
