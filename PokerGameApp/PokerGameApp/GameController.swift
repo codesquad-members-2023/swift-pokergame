e22b869e93fc0e7d7ba19165f65d286426cd2548
SONG <geun2121@gmail.com>
2023-03-23 20:26:35 +0900

    Update PokerGameAppTests class
    - 모든 케이스에 대해 테스트 코드 작성.

--- PokerGameApp/PokerGameApp/GameController.swift
+++ PokerGameApp/PokerGameApp/GameController.swift
@@ -9,15 +9,13 @@ import Foundation
 
 class GameController  {
     
-    func run( mode : GameSetting.Mode , numberOfParticipant : Int ) {
+    func run( mode : GameSetting.Mode , numberOfParticipant : Int ) -> [Player] {
         let gameSetting = GameSetting(mode, numberOfParticipant)
-        gameSetting.setPlayer()
+        gameSetting.setPlayer() // 참가자 numberOfParticipant명과 딜러 1명으로 구성된 playerList를 만든다.
         
         let dealer : Dealer = gameSetting.playerList[numberOfParticipant] as! Dealer
         dealer.work()
         
-        for player in gameSetting.playerList {
-            print(player)
-        }
+        return gameSetting.playerList
     }
 }
--- PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
+++ PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
@@ -9,14 +9,16 @@ import XCTest
 
 @testable import PokerGameApp
 final class PokerGameAppTests: XCTestCase {
-    var sut : GameSetting!
+    var sut1 : GameSetting!
+    var sut2 : GameController!
     
     override func setUpWithError() throws {
         try super.setUpWithError()
     }
     
     override func tearDownWithError() throws {
-        sut = nil
+        sut1 = nil
+        sut2 = nil
         try super.tearDownWithError()
     }
         
@@ -27,16 +29,38 @@ final class PokerGameAppTests: XCTestCase {
         
         for numberOfParticipant in 1...4 {
             // when
-            sut = GameSetting(.fiveStud, numberOfParticipant)
+            sut1 = GameSetting(.fiveStud, numberOfParticipant)
             
-            sut.setPlayer()
+            sut1.setPlayer()
             
-            let participantCount = sut.playerList.filter { $0 is Participant }.count // 타입 체크
-            let dealerCount = sut.playerList.filter { $0 is Dealer }.count
+            let participantCount = sut1.playerList.filter { $0 is Participant }.count // 타입 체크
+            let dealerCount = sut1.playerList.filter { $0 is Dealer }.count
             
             // then
             XCTAssertEqual(participantCount + dealerCount, numberOfParticipant + numberOfdealer ) // 개수 체크
         }
     }
-
+    
+    func testGameController() {
+        
+        sut2 = GameController()
+        
+        for numberOfParticipant in 1...4 {
+            let fiveStudResultPlayerList = sut2.run(mode: .fiveStud, numberOfParticipant: numberOfParticipant)
+            
+            for index in 0...numberOfParticipant {
+                XCTAssertFalse(fiveStudResultPlayerList[index].isHavingProblemInHand5Stud())
+                // 카드 5개가 들어갔는지 , 중복된 카드가 들어갔는지 체크해서 문제가 있으면 true 리턴 , false 일시 테스트 통과
+            }
+        }
+        
+        for numberOfParticipant in 1...4 {
+            let fiveStudResultPlayerList = sut2.run(mode: .sevenStud, numberOfParticipant: numberOfParticipant)
+            
+            for index in 0...numberOfParticipant {
+                XCTAssertFalse(fiveStudResultPlayerList[index].isHavingProblemInHand7Stud())
+                // 카드 7개가 들어갔는지 , 중복된 카드가 들어갔는지 체크해서 문제가 있으면 true 리턴 , false 일시 테스트 통과
+            }
+        }
+    }
 }
