41988824d621887715627e62f7a190b4b5eacbc4
demiiann <tkim0156@gmail.com>
2023-03-27 02:40:21 +0900

    feat: XCTest 파일 생성 및 멤버변수 작성
    - pokerGame 객체 저장할 변수 선언
    -XCTAssertTrue 함수에 들어갈 bool 타입 변수 선언(카드 갯수, 플레이어 인원수, 중복된 카드 여부, 게임끝났는지 여부 확인)
    - 실제로 몇장의 카드가 할당 되었는지 체크하는 변수
    - 예상결과 및 동작예시 에서 "카드게임방식, 인원수를 지정해서 실행"이라고 명시되어 있기 때문에 각각의 변수를 지정

---
+++ PokerGameApp/PokerGameAppTests/PokerGameAppTests.swift
@@ -0,0 +1,20 @@
+//
+//  PokerGameAppTests.swift
+//  PokerGameAppTests
+//
+//  Created by ilim on 2023/03/24.
+//
+
+import XCTest
+@testable import PokerGameApp
+
+final class PokerGameAppTests: XCTestCase {
+    private var sut: PokerGame!
+    private var isValidNumberOfCards = true
+    private var isValidNumberOfPlayers = true
+    private var isThereNoDuplicatedCards = true
+    private var isPokerGameEnded = false
+    private var actualNumberOfCards = 0
+    private let numberOfCards = 5
+    private let numberOfPlayers = 4
+}
