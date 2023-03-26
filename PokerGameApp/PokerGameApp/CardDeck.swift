db81b79b378a051ac3f6ae366deddfa7ada8ab9f
demiiann <tkim0156@gmail.com>
2023-03-27 01:19:06 +0900

    refactor : CardDeck 리팩토링
    - 구조체에서 클래스로 변경
    - 불필요한 주석 삭제
    - removeOne 함수가 nil을 반환할 가능성을 고려하여 옵셔널 적용

--- PokerGameApp/PokerGameApp/CardDeck.swift
+++ PokerGameApp/PokerGameApp/CardDeck.swift
@@ -7,28 +7,30 @@
 
 import Foundation
 
-struct CardDeck {
-    private var cardList = [Card]()
+class CardDeck {
+    private var cardList = [Card?]()
     
     func count() -> Int {
         return cardList.count
     }
     
-    mutating func shuffle() { // Knuth Shuffle 구현
+    func shuffle() {
         for i in 0 ..< (cardList.count - 1) {
             let randomIndex = Int.random(in: i ..< cardList.count)
             cardList.swapAt(i, randomIndex)
         }
     }
     
-    mutating func removeOne() -> Card {
-        let index = cardList.count - 1
-        let removedCard = cardList[index]
-        cardList.removeLast()
+    func removeOne() -> Card? {
+        let index = count() > 0 ? cardList.count - 1 : -1
+        let removedCard = index != -1 ? cardList[index] : nil
+        if removedCard != nil {
+            cardList.removeLast()
+        }
         return removedCard
     }
     
-    mutating func reset() {
+    func reset() {
         if !cardList.isEmpty { cardList.removeAll() }
         for suit in Suits.allCases {
             for rank in Ranks.allCases {
