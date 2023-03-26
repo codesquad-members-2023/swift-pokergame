53e1d3943b14dbaa79645fa04a456e49e0cf216b
demiiann <tkim0156@gmail.com>
2023-03-27 01:54:19 +0900

    feat: Player 파일 생성 및 초기화
    - 플레이어의 이름과 각자의 카드덱 배열 멤버 변수 선언
    - init 함수 생성

---
+++ PokerGameApp/PokerGameApp/Player.swift
@@ -0,0 +1,20 @@
+//
+//  Player.swift
+//  PokerGameApp
+//
+//  Created by ilim on 2023/03/22.
+//
+
+import Foundation
+
+class Player {
+    private var name: String
+    private var cardDeck: [Card?]
+    
+    init(name: String, cardDeck: [Card?]) {
+        self.name = name
+        self.cardDeck = cardDeck
+    }
+    
+    
+}
